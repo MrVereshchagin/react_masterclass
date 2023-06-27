391b6d96d81c8cae367a170ea49220094f25fe15
Igor Vereshchagin <9868425@gmail.com>
2023-06-27 12:28:09 +0300

    feat: make start configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
-import classes from './test.module.scss';
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className={classes.test}>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface AppProps {
+
 }
 
-export default App;
+export function App({}: AppProps) {
+  return(
+    <Routes>
+      <Route>
+
+      </Route>
+    </Routes>
+  )
+}
